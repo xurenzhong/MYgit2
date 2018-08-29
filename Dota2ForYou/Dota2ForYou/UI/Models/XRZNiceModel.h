f39e86b4bee0b4cbb145fbb0e1b2b6da70635a6d
xurenzhong <xrz23942182@163.com>
2018-08-29 17:50:05 +0800

    no message

---
+++ Dota2ForYou/Dota2ForYou/UI/Models/XRZNiceModel.h
@@ -0,0 +1,17 @@
+//
+// Created by XuRenzhong on 16/6/16.
+// Copyright (c) 2016 XuRenzhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface XRZNiceModel : NSObject
+
+@property (nonatomic, copy) NSString *acd;
+
+@property(readonly) NSString *string;
+
+
+@end
+
---
+++ Dota2ForYou/Dota2ForYou/UI/Models/XRZNiceModel.m
@@ -0,0 +1,12 @@
+//
+// Created by XuRenzhong on 16/6/16.
+// Copyright (c) 2016 XuRenzhong. All rights reserved.
+//
+
+#import "XRZNiceModel.h"
+
+
+@implementation XRZNiceModel {
+
+}
+@end
